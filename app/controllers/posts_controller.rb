e52434f259a81abd425411b5ad86c91abce53685
Jinai kakinohana <dogcable@gmail.com>
2015-09-19 17:24:23 +0900

    add post fomr

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,28 @@
 class PostsController < ApplicationController
   def index
-
-  end
-
-  def create
-
+    # @posts = Post.all.order("id desc")
   end
 
   def new
+  end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: '投稿しました'
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def show
-
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
