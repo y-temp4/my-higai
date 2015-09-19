8d6566ed3b5527a91346c22f59d60dcdf796c328
y-temp4 <b1434514@planet.kanazawa-it.ac.jp>
2015-09-19 15:38:17 +0900

    add seed_fu

---
+++ db/fixtures/category.rb
@@ -0,0 +1,5 @@
+Category.seed(:id,
+  { id: 1, name: 'もうやめて' },
+  { id: 2, name: 'ふせぐ' },
+  { id: 3, name: 'きいて' },
+)
---
+++ db/fixtures/post.rb
@@ -0,0 +1,4 @@
+Post.seed(:id,
+  { id: 1, title: 'テスト投稿', body: 'こりごりです。', user_id: 1, category_id: 1 },
+  { id: 2, title: 'テスト投稿2', body: 'aaaaaaa。', user_id: 1, category_id: 1 },
+)
