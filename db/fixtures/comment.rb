a41c168682bd3ee1ba5eeb89b64d4b1cab2a2c8f
j4ve1in <fgm148.j4ve1in@outlook.com>
2015-09-20 05:26:56 +0900

    update seed value of comment

--- db/fixtures/comment.rb
+++ db/fixtures/comment.rb
@@ -1,5 +1,13 @@
-Category.seed(:id,
-  { id: 1, name: 'もうやめて' },
-  { id: 2, name: 'ふせぐ' },
-  { id: 3, name: 'きいて' },
+body = %w(
+  自分も同じような経験があります。頑張って下さい。
+  そんなことがあるんですね。とても参考になります。
+  ああ。うん。
+  そうだね。
+  うーん。
 )
+
+1.upto(100) do |n|
+  Comment.seed(:id,
+    { id: "#{n}", body: body[n%5], user_id: (n%3) +1, post_id: (n%8) +1 },
+  )
+end
