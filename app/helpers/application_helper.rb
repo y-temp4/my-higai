491704f1571bd8e3d7768dc1c9812f979623c1bc
Jinai kakinohana <dogcable@gmail.com>
2015-09-20 11:30:39 +0900

    fix a pankuzu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,14 +7,14 @@ module ApplicationHelper
       if @post.category_id >=1 || @post.category_id <= 3
         breadcrumb_list  << "#{link_to 'トップ', root_path} > "
         breadcrumb_list  << "#{link_to @post.category.name, category_path(@post.category_id)} > "
-        breadcrumb_list  << "#{link_to @post.title}"
+        breadcrumb_list  << '現在のページ'
       end
     end
 
     if controller.controller_name == 'categories'
       if @category.id >=1
         breadcrumb_list  << "#{link_to 'トップ', root_path} > "
-        breadcrumb_list  << "#{link_to @category.name, category_path(@category.id)} "
+        breadcrumb_list  << "#{@category.name} >"
       end
     end
 
