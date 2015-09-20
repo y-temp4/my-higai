0be82d5bff9c309ec048ad0bda4847879b6563ac
Jinai kakinohana <dogcable@gmail.com>
2015-09-20 13:55:34 +0900

    username変更可能に

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :username, :password, :password_confirmation, :current_password) }
   end
 end
